a2c6bb6d17d0277b0ff67082fb6ddd595f28e720
Andrea Bizzotto <bizz84@gmail.com>
2021-04-22 09:27:43 +0100

    Added ClockHand

--- projects/stopwatch_flutter/lib/ui/elapsed_time_text.dart
+++ projects/stopwatch_flutter/lib/ui/elapsed_time_text.dart
@@ -12,7 +12,7 @@ class ElapsedTimeText extends StatelessWidget {
     final hundredsStr = hundreds.toStringAsFixed(0).padLeft(2, '0');
     final secondsStr = seconds.toString().padLeft(2, '0');
     final minutesStr = minutes.toString().padLeft(2, '0');
-    const digitWidth = 24.0;
+    const digitWidth = 12.0;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -40,7 +40,7 @@ class TimeDigit extends StatelessWidget {
       width: width,
       child: Text(
         text,
-        style: TextStyle(fontSize: 40),
+        style: TextStyle(fontSize: 20),
         textAlign: TextAlign.center,
       ),
     );
