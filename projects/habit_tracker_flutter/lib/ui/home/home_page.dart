3b0b3816ef2708b91a76c904a23d29f6bc7a382a
Andrea Bizzotto <bizz84@gmail.com>
2021-04-22 13:58:02 +0100

    Starter project for chapter 3 (disable SVG preload)
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/main.dart

---
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.of(context).primary,
+    );
+  }
+}
