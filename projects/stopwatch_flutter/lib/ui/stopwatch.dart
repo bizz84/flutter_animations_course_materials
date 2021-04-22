a272d5a45d4514337d60a97781cb8dc7bb45f57f
Andrea Bizzotto <bizz84@gmail.com>
2021-04-22 09:25:01 +0100

    Timer implementation

--- projects/stopwatch_flutter/lib/ui/stopwatch.dart
+++ projects/stopwatch_flutter/lib/ui/stopwatch.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
+import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 
 class Stopwatch extends StatefulWidget {
   @override
@@ -7,10 +9,31 @@ class Stopwatch extends StatefulWidget {
 }
 
 class _StopwatchState extends State<Stopwatch> {
+  late DateTime _initialTime;
+  late final Timer _timer;
+  Duration _elapsed = Duration.zero;
+  @override
+  void initState() {
+    super.initState();
+    _initialTime = DateTime.now();
+    _timer = Timer.periodic(Duration(milliseconds: 50), (_) {
+      final now = DateTime.now();
+      setState(() {
+        _elapsed = now.difference(_initialTime);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ElapsedTimeTextBasic(
-      elapsed: Duration(seconds: 5),
+    return ElapsedTimeText(
+      elapsed: _elapsed,
     );
   }
 }
