cc6afde59e430a1efbde24edd1777f4190904abd
Andrea Bizzotto <bizz84@gmail.com>
2021-04-22 09:26:18 +0100

    Add StopwatchRenderer widget

--- projects/stopwatch_flutter/lib/ui/stopwatch.dart
+++ projects/stopwatch_flutter/lib/ui/stopwatch.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:stopwatch_flutter/ui/stopwatch_renderer.dart';
 
 class Stopwatch extends StatefulWidget {
   @override
@@ -30,7 +31,7 @@ class _StopwatchState extends State<Stopwatch>
 
   @override
   Widget build(BuildContext context) {
-    return ElapsedTimeText(
+    return StopwatchRenderer(
       elapsed: _elapsed,
     );
   }
---
+++ projects/stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
+
+class StopwatchRenderer extends StatelessWidget {
+  const StopwatchRenderer({
+    Key? key,
+    required this.elapsed,
+  }) : super(key: key);
+  final Duration elapsed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElapsedTimeText(
+      elapsed: elapsed,
+    );
+  }
+}
