dfd63fa5147570203c26196c7e66ee378f8303de
Andrea Bizzotto <bizz84@gmail.com>
2021-04-30 11:23:09 +0100

    Implement TaskCompletionRing

--- projects/habit_tracker_flutter/lib/ui/task/task_completion_ring.dart
+++ projects/habit_tracker_flutter/lib/ui/task/task_completion_ring.dart
@@ -1,21 +1,65 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 class TaskCompletionRing extends StatelessWidget {
+  const TaskCompletionRing({required this.progress});
+  final double progress;
   @override
   Widget build(BuildContext context) {
+    final themeData = AppTheme.of(context);
     return AspectRatio(
       aspectRatio: 1.0,
       child: CustomPaint(
-        painter: RingPainter(),
+        painter: RingPainter(
+          progress: progress,
+          taskNotCompletedColor: themeData.taskRing,
+          taskCompletedColor: themeData.accent,
+        ),
       ),
     );
   }
 }
 
 class RingPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {}
+  RingPainter({
+    required this.progress,
+    required this.taskNotCompletedColor,
+    required this.taskCompletedColor,
+  });
+  final double progress;
+  final Color taskNotCompletedColor;
+  final Color taskCompletedColor;
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+  void paint(Canvas canvas, Size size) {
+    final strokeWidth = size.width / 15.0;
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = (size.width - strokeWidth) / 2;
+
+    final backgroundPaint = Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = strokeWidth
+      ..color = taskNotCompletedColor
+      ..style = PaintingStyle.stroke;
+    canvas.drawCircle(center, radius, backgroundPaint);
+
+    final foregroundPaint = Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = strokeWidth
+      ..color = taskCompletedColor
+      ..style = PaintingStyle.stroke;
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -pi / 2,
+      2 * pi * progress,
+      false,
+      foregroundPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant RingPainter oldDelegate) =>
+      oldDelegate.progress != progress;
 }
