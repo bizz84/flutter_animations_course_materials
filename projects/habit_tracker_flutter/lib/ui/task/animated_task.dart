983ca5d070c112b3d21c18216a1624030f37e96a
Andrea Bizzotto <bizz84@gmail.com>
2021-04-30 15:44:59 +0100

    Add GestureDetector and CurveTween

--- projects/habit_tracker_flutter/lib/ui/task/animated_task.dart
+++ projects/habit_tracker_flutter/lib/ui/task/animated_task.dart
@@ -11,6 +11,7 @@ class AnimatedTask extends StatefulWidget {
 class _AnimatedTaskState extends State<AnimatedTask>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animationController;
+  late final Animation<double> _curveAnimation;
   @override
   void initState() {
     super.initState();
@@ -18,7 +19,9 @@ class _AnimatedTaskState extends State<AnimatedTask>
       vsync: this,
       duration: const Duration(milliseconds: 750),
     );
-    _animationController.forward();
+    _curveAnimation = _animationController.drive(
+      CurveTween(curve: Curves.easeInOut),
+    );
   }
 
   @override
@@ -27,15 +30,33 @@ class _AnimatedTaskState extends State<AnimatedTask>
     super.dispose();
   }
 
+  void _handleTapDown(TapDownDetails details) {
+    if (_animationController.status != AnimationStatus.completed) {
+      _animationController.forward();
+    } else {
+      _animationController.value = 0.0;
+    }
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    if (_animationController.status != AnimationStatus.completed) {
+      _animationController.reverse();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animationController,
-      builder: (BuildContext context, Widget? child) {
-        return TaskCompletionRing(
-          progress: _animationController.value,
-        );
-      },
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      child: AnimatedBuilder(
+        animation: _curveAnimation,
+        builder: (BuildContext context, Widget? child) {
+          return TaskCompletionRing(
+            progress: _curveAnimation.value,
+          );
+        },
+      ),
     );
   }
 }
