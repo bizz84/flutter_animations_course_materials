38d9f012909bf8aca316184f1c2fe642d4be16aa
Andrea Bizzotto <bizz84@gmail.com>
2021-05-03 11:03:18 +0100

    Add TaskWithName

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker_flutter/constants/app_assets.dart';
-import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
+import 'package:habit_tracker_flutter/models/task_preset.dart';
+import 'package:habit_tracker_flutter/ui/task/task_with_name.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,8 +14,8 @@ class HomePage extends StatelessWidget {
       body: const Center(
         child: SizedBox(
           width: 240,
-          child: AnimatedTask(
-            iconName: AppAssets.dog,
+          child: TaskWithName(
+            task: TaskPreset(name: 'Walk the Dog', iconName: AppAssets.dog),
           ),
         ),
       ),
