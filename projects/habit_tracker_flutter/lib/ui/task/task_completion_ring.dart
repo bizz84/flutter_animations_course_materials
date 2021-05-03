1c9b30124130a3c39fb195d19868acb03675c1f1
Andrea Bizzotto <bizz84@gmail.com>
2021-05-03 09:04:00 +0100

    Tweak TaskCompletionRing

--- projects/habit_tracker_flutter/lib/ui/task/task_completion_ring.dart
+++ projects/habit_tracker_flutter/lib/ui/task/task_completion_ring.dart
@@ -34,22 +34,26 @@ class RingPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    final notCompleted = progress < 1.0;
     final strokeWidth = size.width / 15.0;
     final center = Offset(size.width / 2, size.height / 2);
-    final radius = (size.width - strokeWidth) / 2;
+    final radius =
+        notCompleted ? (size.width - strokeWidth) / 2 : size.width / 2;
 
-    final backgroundPaint = Paint()
-      ..isAntiAlias = true
-      ..strokeWidth = strokeWidth
-      ..color = taskNotCompletedColor
-      ..style = PaintingStyle.stroke;
-    canvas.drawCircle(center, radius, backgroundPaint);
+    if (notCompleted) {
+      final backgroundPaint = Paint()
+        ..isAntiAlias = true
+        ..strokeWidth = strokeWidth
+        ..color = taskNotCompletedColor
+        ..style = PaintingStyle.stroke;
+      canvas.drawCircle(center, radius, backgroundPaint);
+    }
 
     final foregroundPaint = Paint()
       ..isAntiAlias = true
       ..strokeWidth = strokeWidth
       ..color = taskCompletedColor
-      ..style = PaintingStyle.stroke;
+      ..style = notCompleted ? PaintingStyle.stroke : PaintingStyle.fill;
     canvas.drawArc(
       Rect.fromCircle(center: center, radius: radius),
       -pi / 2,
