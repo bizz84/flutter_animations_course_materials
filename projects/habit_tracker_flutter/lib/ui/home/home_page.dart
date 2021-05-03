43406521fe50d64416ddf781754e1ced5e055b24
Andrea Bizzotto <bizz84@gmail.com>
2021-05-03 14:42:34 +0100

    Finished project for chapter 4
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/ui/home/home_page.dart
    #	projects/habit_tracker_flutter/lib/ui/task/task_with_name.dart
    
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/ui/home/tasks_grid.dart

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -11,17 +11,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.of(context).primary,
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-        child: TasksGrid(
-          tasks: [
-            TaskPreset(name: 'Eat a Healthy Meal', iconName: AppAssets.carrot),
-            TaskPreset(name: 'Walk the Dog', iconName: AppAssets.dog),
-            TaskPreset(name: 'Do Some Coding', iconName: AppAssets.html),
-            TaskPreset(name: 'Meditate', iconName: AppAssets.meditation),
-            TaskPreset(name: 'Do 10 Pushups', iconName: AppAssets.pushups),
-            TaskPreset(name: 'Sleep 8 Hours', iconName: AppAssets.rest),
-          ],
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+          child: TasksGrid(
+            tasks: [
+              TaskPreset(
+                  name: 'Eat a Healthy Meal', iconName: AppAssets.carrot),
+              TaskPreset(name: 'Walk the Dog', iconName: AppAssets.dog),
+              TaskPreset(name: 'Do Some Coding', iconName: AppAssets.html),
+              TaskPreset(name: 'Meditate', iconName: AppAssets.meditation),
+              TaskPreset(name: 'Do 10 Pushups', iconName: AppAssets.pushups),
+              TaskPreset(name: 'Sleep 8 Hours', iconName: AppAssets.rest),
+            ],
+          ),
         ),
       ),
     );
