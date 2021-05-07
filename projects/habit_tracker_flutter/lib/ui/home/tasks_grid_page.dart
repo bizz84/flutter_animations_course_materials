1bca5437c78d111370de2a91e657f8722713092d
Andrea Bizzotto <bizz84@gmail.com>
2021-05-07 10:03:59 +0100

    Starter project chapter 5
    # Conflicts:
    #	projects/habit_tracker_flutter/ios/Runner.xcodeproj/project.pbxproj
    #	projects/habit_tracker_flutter/lib/ui/home/home_page.dart
    #	projects/habit_tracker_flutter/pubspec.yaml

---
+++ projects/habit_tracker_flutter/lib/ui/home/tasks_grid_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/models/task_preset.dart';
+import 'package:habit_tracker_flutter/ui/home/tasks_grid.dart';
+import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+
+class TasksGridPage extends StatelessWidget {
+  const TasksGridPage({super.key, required this.tasks});
+  final List<TaskPreset> tasks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.of(context).primary,
+      body: SafeArea(
+        child: TasksGridContents(
+          tasks: tasks,
+        ),
+      ),
+    );
+  }
+}
+
+class TasksGridContents extends StatelessWidget {
+  const TasksGridContents({super.key, required this.tasks});
+  final List<TaskPreset> tasks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+      child: TasksGrid(
+        tasks: tasks,
+      ),
+    );
+  }
+}
