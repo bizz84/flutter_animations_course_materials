bf965a2a8a9a4192edcd1a245ae8ea8f91f830e6
Andrea Bizzotto <bizz84@gmail.com>
2021-05-07 10:03:59 +0100

    Starter project chapter 5
    # Conflicts:
    #	projects/habit_tracker_flutter/ios/Runner.xcodeproj/project.pbxproj
    #	projects/habit_tracker_flutter/lib/ui/home/home_page.dart
    #	projects/habit_tracker_flutter/pubspec.yaml

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -1,32 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker_flutter/constants/app_assets.dart';
 import 'package:habit_tracker_flutter/models/task_preset.dart';
-import 'package:habit_tracker_flutter/ui/home/tasks_grid.dart';
-import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+import 'package:habit_tracker_flutter/ui/home/tasks_grid_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppTheme.of(context).primary,
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-          child: TasksGrid(
-            tasks: [
-              TaskPreset(
-                  name: 'Eat a Healthy Meal', iconName: AppAssets.carrot),
-              TaskPreset(name: 'Walk the Dog', iconName: AppAssets.dog),
-              TaskPreset(name: 'Do Some Coding', iconName: AppAssets.html),
-              TaskPreset(name: 'Meditate', iconName: AppAssets.meditation),
-              TaskPreset(name: 'Do 10 Pushups', iconName: AppAssets.pushups),
-              TaskPreset(name: 'Sleep 8 Hours', iconName: AppAssets.rest),
-            ],
-          ),
-        ),
-      ),
+    return const TasksGridPage(
+      tasks: [
+        TaskPreset(name: 'Eat a Healthy Meal', iconName: AppAssets.carrot),
+        TaskPreset(name: 'Walk the Dog', iconName: AppAssets.dog),
+        TaskPreset(name: 'Do Some Coding', iconName: AppAssets.html),
+        TaskPreset(name: 'Meditate', iconName: AppAssets.meditation),
+        TaskPreset(name: 'Do 10 Pushups', iconName: AppAssets.pushups),
+        TaskPreset(name: 'Sleep 8 Hours', iconName: AppAssets.rest),
+      ],
     );
   }
 }
