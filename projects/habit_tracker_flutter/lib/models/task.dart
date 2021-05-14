520ebe00882cb264faeb507cab0ee6cf4dca3bae
Andrea Bizzotto <bizz84@gmail.com>
2021-05-14 10:53:00 +0100

    Add Task class

---
+++ projects/habit_tracker_flutter/lib/models/task.dart
@@ -0,0 +1,13 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class Task {
+  Task({required this.id, required this.name, required this.iconName});
+
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String name;
+  @HiveField(2)
+  final String iconName;
+}
