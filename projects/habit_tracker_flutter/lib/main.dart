3cdaa791923fbee248364c5c423c1d529a900570
Andrea Bizzotto <bizz84@gmail.com>
2021-05-14 11:57:38 +0100

    Create HiveDataStore and initialize it in main()
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/main.dart

--- projects/habit_tracker_flutter/lib/main.dart
+++ projects/habit_tracker_flutter/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker_flutter/constants/app_colors.dart';
+import 'package:habit_tracker_flutter/persistence/hive_data_store.dart';
 import 'package:habit_tracker_flutter/ui/home/home_page.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final dataStore = HiveDataStore();
+  await dataStore.init();
   runApp(const MyApp());
 }
 
---
+++ projects/habit_tracker_flutter/lib/persistence/hive_data_store.dart
@@ -0,0 +1,14 @@
+import 'package:habit_tracker_flutter/models/task.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HiveDataStore {
+  static const tasksBoxName = 'tasks';
+
+  Future<void> init() async {
+    await Hive.initFlutter();
+    // register adapters
+    Hive.registerAdapter<Task>(TaskAdapter());
+    // open boxes
+    await Hive.openBox<Task>(tasksBoxName);
+  }
+}
