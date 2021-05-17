d21ecadde231012465b37c89856b5377cfb93f19
Andrea Bizzotto <bizz84@gmail.com>
2021-05-17 09:55:51 +0100

    Add Task.create and force argument
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/main.dart

--- projects/habit_tracker_flutter/lib/models/task.dart
+++ projects/habit_tracker_flutter/lib/models/task.dart
@@ -1,10 +1,16 @@
 import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
 
 part 'task.g.dart';
 
 @HiveType(typeId: 0)
 class Task {
   Task({required this.id, required this.name, required this.iconName});
+
+  factory Task.create({required String name, required String iconName}) {
+    final id = const Uuid().v1();
+    return Task(id: id, name: name, iconName: iconName);
+  }
 
   @HiveField(0)
   final String id;
