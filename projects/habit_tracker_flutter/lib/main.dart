a03854a87b42505b6c984487d97b458cfb511bc8
Andrea Bizzotto <bizz84@gmail.com>
2021-05-17 11:12:15 +0100

    Add ValueListenableBuilder code
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/ui/home/home_page.dart
    #	projects/habit_tracker_flutter/lib/ui/home/tasks_grid.dart
    #	projects/habit_tracker_flutter/lib/ui/home/tasks_grid_page.dart
    #	projects/habit_tracker_flutter/lib/ui/task/task_with_name.dart

--- projects/habit_tracker_flutter/lib/main.dart
+++ projects/habit_tracker_flutter/lib/main.dart
@@ -12,12 +12,12 @@ Future<void> main() async {
   await dataStore.init();
   await dataStore.createDemoTasks(
     tasks: [
-      Task.create(name: 'Eat a Healthy Meal', iconName: AppAssets.carrot),
-      Task.create(name: 'Walk the Dog', iconName: AppAssets.dog),
-      Task.create(name: 'Do Some Coding', iconName: AppAssets.html),
-      Task.create(name: 'Meditate', iconName: AppAssets.meditation),
-      Task.create(name: 'Do 10 Pushups', iconName: AppAssets.pushups),
-      Task.create(name: 'Sleep 8 Hours', iconName: AppAssets.rest),
+      Task.create(name: 'Take Vitamins', iconName: AppAssets.vitamins),
+      Task.create(name: 'Cycle to Work', iconName: AppAssets.bike),
+      Task.create(name: 'Wash Your Hands', iconName: AppAssets.washHands),
+      Task.create(name: 'Wear a Mask', iconName: AppAssets.mask),
+      Task.create(name: 'Brush Your Teeth', iconName: AppAssets.toothbrush),
+      Task.create(name: 'Floss Your Teeth', iconName: AppAssets.dentalFloss),
     ],
     force: false,
   );
--- projects/habit_tracker_flutter/lib/ui/task/task_with_name.dart
+++ projects/habit_tracker_flutter/lib/ui/task/task_with_name.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker_flutter/constants/text_styles.dart';
-import 'package:habit_tracker_flutter/models/task_preset.dart';
+import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 class TaskWithName extends StatelessWidget {
   const TaskWithName({super.key, required this.task});
-  final TaskPreset task;
+  final Task task;
 
   @override
   Widget build(BuildContext context) {
