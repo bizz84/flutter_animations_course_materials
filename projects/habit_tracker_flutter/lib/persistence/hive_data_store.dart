f98a62dddb6b3fb703486775a9a9164954aaf793
Andrea Bizzotto <bizz84@gmail.com>
2021-06-07 12:49:30 +0100

    Add and use dataStoreProvider
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/main.dart
    #	projects/habit_tracker_flutter/lib/ui/home/home_page.dart

--- projects/habit_tracker_flutter/lib/persistence/hive_data_store.dart
+++ projects/habit_tracker_flutter/lib/persistence/hive_data_store.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -30,3 +31,7 @@ class HiveDataStore {
     return Hive.box<Task>(tasksBoxName).listenable();
   }
 }
+
+final dataStoreProvider = Provider<HiveDataStore>((ref) {
+  return HiveDataStore();
+});
