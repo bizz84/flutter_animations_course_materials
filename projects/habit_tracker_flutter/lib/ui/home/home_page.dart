681b82c2b47a7e7444279a11fd4f5a9cec231e64
Andrea Bizzotto <bizz84@gmail.com>
2021-06-07 12:49:30 +0100

    Add and use dataStoreProvider
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/main.dart
    #	projects/habit_tracker_flutter/lib/ui/home/home_page.dart

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:habit_tracker_flutter/persistence/hive_data_store.dart';
 import 'package:habit_tracker_flutter/ui/home/tasks_grid_page.dart';
 import 'package:hive/hive.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final dataStore = HiveDataStore();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final dataStore = ref.watch(dataStoreProvider);
     return ValueListenableBuilder(
       valueListenable: dataStore.tasksListenable(),
       builder: (_, Box<Task> box, __) => TasksGridPage(
