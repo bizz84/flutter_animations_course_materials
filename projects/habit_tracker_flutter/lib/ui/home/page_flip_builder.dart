5236b4f312d8192b6e20cca6d8e0b87a7022dfc1
Andrea Bizzotto <bizz84@gmail.com>
2021-06-14 12:28:45 +0100

    AnimationController setup code
    # Conflicts:
    #	projects/habit_tracker_flutter/lib/ui/home/page_flip_builder.dart

--- projects/habit_tracker_flutter/lib/ui/home/page_flip_builder.dart
+++ projects/habit_tracker_flutter/lib/ui/home/page_flip_builder.dart
@@ -13,10 +13,40 @@ class PageFlipBuilder extends StatefulWidget {
   PageFlipBuilderState createState() => PageFlipBuilderState();
 }
 
-class PageFlipBuilderState extends State<PageFlipBuilder> {
+class PageFlipBuilderState extends State<PageFlipBuilder>
+    with SingleTickerProviderStateMixin {
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 500),
+  );
+  bool _showFrontSide = true;
+
+  @override
+  void initState() {
+    _controller.addStatusListener(_updateStatus);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.removeStatusListener(_updateStatus);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _updateStatus(AnimationStatus status) {
+    if (status == AnimationStatus.completed ||
+        status == AnimationStatus.dismissed) {
+      setState(() => _showFrontSide = !_showFrontSide);
+    }
+  }
+
   void flip() {
-    // TODO: Update state
-    debugPrint('flip called');
+    if (_showFrontSide) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
   }
 
   @override
