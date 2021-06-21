f07abe29e403cf3ff9520362ab4c822100247065
Andrea Bizzotto <bizz84@gmail.com>
2021-06-21 09:02:44 +0100

    Add separate ValueKeys + fix GlobalKey

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -6,30 +6,40 @@ import 'package:habit_tracker_flutter/ui/home/tasks_grid_page.dart';
 import 'package:hive/hive.dart';
 import 'package:page_flip_builder/page_flip_builder.dart';
 
-class HomePage extends ConsumerWidget {
-  final _pageFlipKey = GlobalKey<PageFlipBuilderState>();
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final dataStore = ref.watch(dataStoreProvider);
-    return Container(
-      color: Colors.black,
-      child: PageFlipBuilder(
-        key: _pageFlipKey,
-        frontBuilder: (_) => ValueListenableBuilder(
-          valueListenable: dataStore.frontTasksListenable(),
-          builder: (_, Box<Task> box, __) => TasksGridPage(
-            tasks: box.values.toList(),
-            onFlip: () => _pageFlipKey.currentState?.flip(),
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _pageFlipKey = GlobalKey<PageFlipBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (_, ref, __) {
+      final dataStore = ref.watch(dataStoreProvider);
+      return Container(
+        color: Colors.black,
+        child: PageFlipBuilder(
+          key: _pageFlipKey,
+          frontBuilder: (_) => ValueListenableBuilder(
+            valueListenable: dataStore.frontTasksListenable(),
+            builder: (_, Box<Task> box, __) => TasksGridPage(
+              key: ValueKey(1),
+              tasks: box.values.toList(),
+              onFlip: () => _pageFlipKey.currentState?.flip(),
+            ),
+          ),
+          backBuilder: (_) => ValueListenableBuilder(
+            valueListenable: dataStore.backTasksListenable(),
+            builder: (_, Box<Task> box, __) => TasksGridPage(
+              key: ValueKey(2),
+              tasks: box.values.toList(),
+              onFlip: () => _pageFlipKey.currentState?.flip(),
+            ),
           ),
         ),
-        backBuilder: (_) => ValueListenableBuilder(
-          valueListenable: dataStore.backTasksListenable(),
-          builder: (_, Box<Task> box, __) => TasksGridPage(
-            tasks: box.values.toList(),
-            onFlip: () => _pageFlipKey.currentState?.flip(),
-          ),
-        ),
-      ),
-    );
+      );
+    });
   }
 }
