3599fc37f6b2f129fa7f783f9c90ff5bb54c5e1a
Andrea Bizzotto <bizz84@gmail.com>
2021-06-22 13:37:01 +0100

    Add sliding panels with close button and list of colors

--- projects/habit_tracker_flutter/lib/ui/home/tasks_grid_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/tasks_grid_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/models/app_theme_settings.dart';
 import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:habit_tracker_flutter/ui/home/home_page_bottom_options.dart';
 import 'package:habit_tracker_flutter/ui/home/tasks_grid.dart';
+import 'package:habit_tracker_flutter/ui/sliding_panel/sliding_panel.dart';
+import 'package:habit_tracker_flutter/ui/sliding_panel/theme_selection_close.dart';
+import 'package:habit_tracker_flutter/ui/sliding_panel/theme_selection_list.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 class TasksGridPage extends StatelessWidget {
@@ -18,9 +22,38 @@ class TasksGridPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppTheme.of(context).primary,
       body: SafeArea(
-        child: TasksGridContents(
-          tasks: tasks,
-          onFlip: onFlip,
+        child: Stack(
+          children: [
+            TasksGridContents(
+              tasks: tasks,
+              onFlip: onFlip,
+            ),
+            Positioned(
+              bottom: 6,
+              left: 0,
+              width: SlidingPanel.leftPanelFixedWidth,
+              child: SlidingPanel(
+                direction: SlideDirection.leftToRight,
+                child: ThemeSelectionClose(),
+              ),
+            ),
+            Positioned(
+              bottom: 6,
+              right: 0,
+              width: MediaQuery.of(context).size.width -
+                  SlidingPanel.leftPanelFixedWidth,
+              child: SlidingPanel(
+                direction: SlideDirection.rightToLeft,
+                child: ThemeSelectionList(
+                  currentThemeSettings:
+                      AppThemeSettings(colorIndex: 0, variantIndex: 0),
+                  availableWidth: MediaQuery.of(context).size.width -
+                      SlidingPanel.leftPanelFixedWidth -
+                      SlidingPanel.paddingWidth,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
