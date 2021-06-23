5fd0ed5f799464a04f99d5bce11391c95d64c915
Andrea Bizzotto <bizz84@gmail.com>
2021-06-23 11:32:54 +0100

    Add AppThemeManager

---
+++ projects/habit_tracker_flutter/lib/ui/theming/app_theme_manager.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:habit_tracker_flutter/models/app_theme_settings.dart';
+import 'package:habit_tracker_flutter/models/front_or_back_side.dart';
+import 'package:habit_tracker_flutter/persistence/hive_data_store.dart';
+
+class AppThemeManager extends StateNotifier<AppThemeSettings> {
+  AppThemeManager({
+    required AppThemeSettings themeSettings,
+    required this.dataStore,
+    required this.side,
+  }) : super(themeSettings);
+  final HiveDataStore dataStore;
+  final FrontOrBackSide side;
+
+  void updateColorIndex(int colorIndex) {
+    state = state.copyWith(colorIndex: colorIndex);
+    dataStore.setAppThemeSettings(settings: state, side: side);
+  }
+
+  void updateVariantIndex(int variantIndex) {
+    state = state.copyWith(variantIndex: variantIndex);
+    dataStore.setAppThemeSettings(settings: state, side: side);
+  }
+}
