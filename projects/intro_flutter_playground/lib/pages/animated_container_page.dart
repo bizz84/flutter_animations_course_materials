1b2283cb2823f71f92b04f4ce7bffa2e0f14ad23
Andrea Bizzotto <codewithandrea@gmail.com>
2023-12-08 14:06:21 +0000

    Starter project for implicit animations playground

--- projects/intro_flutter_playground/lib/pages/animated_container_page.dart
+++ projects/intro_flutter_playground/lib/pages/animated_container_page.dart
@@ -1,37 +1,3 @@
-/*
-Note: this code sample was taken and adapted from this page:
-https://flutter.dev/docs/cookbook/animation/animated-container
-
-The following BSD license applies:
-
-Copyright 2012, the project authors. All rights reserved. Redistribution and use
-in source and binary forms, with or without modification, are permitted provided
-that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of Google Inc. nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class AnimatedContainerPage extends StatefulWidget {
@@ -42,43 +8,19 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
-  // Define the various properties with default values. Update these properties
-  // when the user taps a FloatingActionButton.
-  double _width = 200;
-  double _height = 200;
-  Color _color = Colors.red;
-  BorderRadiusGeometry _borderRadius = BorderRadius.circular(16);
-
-  // Create a random number generator.
-  final random = Random();
-
-  void _randomize() {
-    setState(() {
-      // Generate a random width and height.
-      _width = random.nextInt(300).toDouble();
-      _height = random.nextInt(300).toDouble();
-
-      // Generate a random color.
-      _color = Color.fromRGBO(
-        random.nextInt(256),
-        random.nextInt(256),
-        random.nextInt(256),
-        1,
-      );
-
-      // Generate a random border radius.
-      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
-    });
-  }
+  final double _width = 200;
+  final double _height = 200;
+  final Color _color = Colors.red;
+  final BorderRadiusGeometry _borderRadius = BorderRadius.circular(16);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AnimatedContainer'),
+        title: Text('AnimatedContainer'),
       ),
       body: Center(
-        child: AnimatedContainer(
+        child: Container(
           // Use the properties stored in the State class.
           width: _width,
           height: _height,
@@ -86,15 +28,7 @@ class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
             color: _color,
             borderRadius: _borderRadius,
           ),
-          // Define how long the animation should take.
-          duration: const Duration(seconds: 1),
-          // Provide an optional curve to make the animation feel smoother.
-          curve: Curves.fastOutSlowIn, // fastOutSlowIn
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _randomize,
-        child: const Icon(Icons.play_arrow),
       ),
     );
   }
--- projects/intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
+++ projects/intro_flutter_playground/lib/pages/tween_animation_builder_page.dart
@@ -7,9 +7,9 @@ class TweenAnimationBuilderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TweenAnimationBuilder'),
+        title: Text('TweenAnimationBuilder'),
       ),
-      body: const Center(
+      body: Center(
         child: HSVColorSelector(),
       ),
     );
@@ -17,66 +17,13 @@ class TweenAnimationBuilderPage extends StatelessWidget {
 }
 
 class HSVColorSelector extends StatefulWidget {
-  const HSVColorSelector({super.key});
-
   @override
   State<HSVColorSelector> createState() => _HSVColorSelectorState();
 }
 
 class _HSVColorSelectorState extends State<HSVColorSelector> {
-  double _hue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AnimatedContainer(
-          color: HSVColor.fromAHSV(1.0, _hue, 1.0, 1.0).toColor(),
-          duration: const Duration(milliseconds: 1500),
-          width: 200,
-          height: 200,
-        ),
-        const SizedBox(height: 48.0),
-        TweenAnimationBuilder<double>(
-          tween: Tween<double>(
-            begin: 0.0,
-            end: _hue,
-          ),
-          duration: const Duration(milliseconds: 1500),
-          builder: (context, hue, child) {
-            final hsvColor = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0);
-            return Container(
-              width: 200,
-              height: 200,
-              color: hsvColor.toColor(),
-            );
-          },
-        ),
-        const SizedBox(height: 48.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Container(
-            height: 30.0,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  for (var h = 0.0; h < 360.0; h += 1)
-                    HSVColor.fromAHSV(1.0, h, 1.0, 1.0).toColor()
-                ],
-                stops: [
-                  for (var h = 0.0; h < 360.0; h += 1) h / 360.0,
-                ],
-              ),
-            ),
-          ),
-        ),
-        Slider.adaptive(
-          value: _hue,
-          min: 0,
-          max: 360,
-          onChanged: (newValue) => setState(() => _hue = newValue),
-        ),
-      ],
-    );
+    return Container();
   }
 }
